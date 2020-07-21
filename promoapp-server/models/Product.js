b02ed1bc62b5f20e7e165e4780832619b9d914a2
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2020-07-21 23:11:02 +0530

    Product model created

---
+++ promoapp-server/models/Product.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  slug: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: String,
+  image: String,
+  description: String,
+  rating: Number,
+  //what students learn
+  wsl: [{ type: mongoose.Schema.Types.Mixed, value: String }],
+  requirements: [{ type: mongoose.Schema.Types.Mixed, value: String }],
+  promoVideoLink: String,
+  productLink: String,
+  price: Number,
+  discountedPrice: Number,
+  status: {
+    type: String,
+    enum: ["active", "inactive", "deleted", "published"],
+    default: "active",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
