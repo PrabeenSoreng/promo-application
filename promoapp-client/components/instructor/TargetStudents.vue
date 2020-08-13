05fb723129b2042c36e4e8a9cbb3043c05f8dea4
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2020-08-13 18:32:23 +0530

    multiline input component added

--- promoapp-client/components/instructor/TargetStudents.vue
+++ promoapp-client/components/instructor/TargetStudents.vue
@@ -5,25 +5,18 @@
     </header>
     <div class="card-content card-section">
       <form>
-        <div class="field">
-          <div class="control">
-            <input
-              class="input is-large"
-              type="text"
-              placeholder="What will students learn in your course ?"
-            />
-          </div>
-        </div>
-        <div class="field">
-          <div class="control">
-            <input
-              class="input is-large"
-              type="text"
-              placeholder="What are the requirements for the course ?"
-            />
-          </div>
-        </div>
+        <MultiLineTextInput label="What will students learn" />
+        <MultiLineTextInput label="What are the requirements" />
       </form>
     </div>
   </div>
 </template>
+
+<script>
+import MultiLineTextInput from "~/components/form/MultiLineTextInput";
+export default {
+  components: {
+    MultiLineTextInput,
+  },
+};
+</script>
