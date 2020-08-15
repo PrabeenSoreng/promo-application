cc406c804ad917032749262a4c46da5f9e8ad12c
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2020-08-15 19:49:47 +0530

    update status and price component

--- promoapp-client/components/instructor/Status.vue
+++ promoapp-client/components/instructor/Status.vue
@@ -7,7 +7,7 @@
       <div class="field">
         <label class="label">Status</label>
         <div class="select is-medium">
-          <select>
+          <select :value="course.status" @change="($event) => emitCourseValue($event, 'status')">
             <option value="default">Change Status</option>
             <option value="active">Active</option>
             <option value="published">Published</option>
@@ -17,3 +17,19 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    course: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    emitCourseValue(e, field) {
+      this.$emit("courseValueUpdated", { value: e.target.value, field });
+    },
+  },
+};
+</script>
